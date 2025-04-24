f8ae4cca2f5969cd0b254b829e4935f002e7dbad
kdannenberg <skattkista@outlook.de>
2025-04-24 17:02:02 +0200

    Fix linting

--- bin/combine_tsv.sh
+++ bin/combine_tsv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# author: Olivia Andersson, modified by Katharina Dannenberg 
+# author: Olivia Andersson, modified by Katharina Dannenberg
 # parameters
 # $@: all files to concatenate
-# concatenated file will be outpur to stdout, make tsv by collecting to file, e.g. combine_tsv ... > combined.tsv 
+# concatenated file will be outpur to stdout, make tsv by collecting to file, e.g. combine_tsv ... > combined.tsv
 
 # input directory
 # tsv_dir="/mnt/anvil/Olivia/16s_internutveckling_CG38/gms16s/outdir_internutveckling_gms16s/results"
@@ -29,13 +29,13 @@ header_added=false
 
 for tsv in $@; do
 # echo "$tsv"
-  if [ "$header_added" = false ]; then
-    head -n 1 "$tsv" | awk '{print $0 "\tSource_File"}'
-    tail -n +2 "$tsv" | awk -v fname=$(basename "$tsv") '{print $0 "\t" fname}'
-    header_added=true
-  else
-     tail -n +2 "$tsv" | awk -v fname=$(basename "$tsv") '{print $0 "\t" fname}'
-  fi
+    if [ "$header_added" = false ]; then
+        head -n 1 "$tsv" | awk '{print $0 "\tSource_File"}'
+        tail -n +2 "$tsv" | awk -v fname=$(basename "$tsv") '{print $0 "\t" fname}'
+        header_added=true
+    else
+        tail -n +2 "$tsv" | awk -v fname=$(basename "$tsv") '{print $0 "\t" fname}'
+    fi
 done
 
 # echo "Combined results saved to $output_file"
