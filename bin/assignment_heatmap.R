5090b34e17b1df84f69c048b10a3d0d8a8d711e8
Frans Wallin <frans.wallin@regionorebrolan.se>
2025-08-19 09:01:51 +0200

    new version of r script using getopt

--- bin/assignment_heatmap.R
+++ bin/assignment_heatmap.R
@@ -1,35 +1,52 @@
 #!/usr/bin/env Rscript
-
 suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(getopt))
 
-version <- "0.0.2"
+version <- "0.0.3"
 
-# Get command-line arguments
-args <- commandArgs(trailingOnly = TRUE)
+# Define options
+spec <- matrix(c(
+  'help'       , 'h', 0, "logical",
+  'version'    , 'v', 0, "logical",
+  'input_file' , NA , 1, "character",
+  'output_file', NA , 1, "character"
+), byrow=TRUE, ncol=4)
 
-# Handle --version
-if ("--version" %in% args) {
-    cat("assignment_heatmap.R version", version, "\n")
-    quit(status = 0)
-}
+# Parse
+opt <- getopt(spec)
 
-# Print help if too few args or help flag is used
-if (length(args) < 2 || "--help" %in% args || "-h" %in% args) {
-    cat("
+# help
+if (!is.null(opt$help)) {
+  cat("
 Usage:
-    Rscript script.R <input_file.tsv> <output_file.png>
+    Rscript script.R [options] --input_file <likelihood tsv file from EMU> --output_file <file.png>
 
 Options:
-    --version     Print version and exit
-    --help, -h    Show this help message
+    -h, --help            Show this help message
+    -v, --version         Print version and exit
+        --input_file      Input file (required)
+        --output_file     Output file (required)
 \n")
-    quit(status = 1)
+  quit(status=0)
 }
 
-# Assign positional arguments
-input_file <- args[1]
-output_file <- args[2]
+# version
+if (!is.null(opt$version)) {
+  cat("assignment_heatmap.R version", version, "\n")
+  quit(status=0)
+}
+
+# Validate required arguments
+if (is.null(opt$input_file) || is.null(opt$output_file)) {
+  cat("Error: --input_file and --output_file are required\n\n")
+  cat(getopt(spec, usage=TRUE))
+  quit(status=1)
+}
+
+input_file  <- opt$input_file
+output_file <- opt$output_file
+
 
 # Read the data
 likelihood_df <- read.table(input_file, sep = "\t", header = TRUE, row.names = 1, check.names = FALSE)
@@ -40,14 +57,14 @@ melted_data <- melt(likelihood_data, id.vars = "ReadID")
 
 # Create the plot
 p <- ggplot(melted_data, aes(x = ReadID, y = variable, fill = value)) +
-    geom_tile() +
-    scale_fill_gradient(low = "white", high = "blue") +
-    labs(x = "Reads", y = "Taxon name", fill = "Likelihood") +
-    theme(
-        axis.text.x = element_blank(),
-        axis.text.y = element_text(angle = 0, vjust = 0.5, hjust = 1),
-        axis.ticks.x = element_blank()
-    )
+  geom_tile() +
+  scale_fill_gradient(low = "white", high = "blue") +
+  labs(x = "Reads", y = "Taxon name", fill = "Likelihood") +
+  theme(
+    axis.text.x = element_blank(),
+    axis.text.y = element_text(angle = 0, vjust = 0.5, hjust = 1),
+    axis.ticks.x = element_blank()
+  )
 
 
 # Save to PNG
